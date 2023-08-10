a35e02c67d2439d4b2955ba1a07968652eced309
Ernesto Campohermoso <ecampohermoso@cirrus-it.net>
2023-08-10 19:33:48 -0400

    Subiendo modificaciones

--- src/main/java/bo/edu/ucb/sis213/App.java
+++ src/main/java/bo/edu/ucb/sis213/App.java
@@ -1,13 +1,125 @@
 package bo.edu.ucb.sis213;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private static final int PIN_CORRECTO = 1234;  // Puedes cambiar este valor al PIN que prefieras
+    private static double saldo = 1000.0;  // Saldo inicial
+    private static int pinActual = PIN_CORRECTO;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int intentos = 3;
+
+        System.out.println("Bienvenido al Cajero Automático.");
+        while (intentos > 0) {
+            System.out.print("Ingrese su PIN de 4 dígitos: ");
+            int pinIngresado = scanner.nextInt();
+            if (pinIngresado == pinActual) {
+                mostrarMenu();
+                break;
+            } else {
+                intentos--;
+                if (intentos > 0) {
+                    System.out.println("PIN incorrecto. Le quedan " + intentos + " intentos.");
+                } else {
+                    System.out.println("PIN incorrecto. Ha excedido el número de intentos.");
+                    System.exit(0);
+                }
+            }
+        }
+    }
+
+    public static void mostrarMenu() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("\nMenú Principal:");
+            System.out.println("1. Consultar saldo.");
+            System.out.println("2. Realizar un depósito.");
+            System.out.println("3. Realizar un retiro.");
+            System.out.println("4. Cambiar PIN.");
+            System.out.println("5. Salir.");
+            System.out.print("Seleccione una opción: ");
+            int opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    consultarSaldo();
+                    break;
+                case 2:
+                    realizarDeposito();
+                    break;
+                case 3:
+                    realizarRetiro();
+                    break;
+                case 4:
+                    cambiarPIN();
+                    break;
+                case 5:
+                    System.out.println("Gracias por usar el cajero. ¡Hasta luego!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intente nuevamente.");
+            }
+        }
+    }
+
+    public static void consultarSaldo() {
+        System.out.println("Su saldo actual es: $" + saldo);
+    }
+
+    public static void realizarDeposito() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese la cantidad a depositar: $");
+        double cantidad = scanner.nextDouble();
+
+        if (cantidad <= 0) {
+            System.out.println("Cantidad no válida.");
+        } else {
+            saldo += cantidad;
+            System.out.println("Depósito realizado con éxito. Su nuevo saldo es: $" + saldo);
+        }
+    }
+
+    public static void realizarRetiro() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese la cantidad a retirar: $");
+        double cantidad = scanner.nextDouble();
+
+        if (cantidad <= 0) {
+            System.out.println("Cantidad no válida.");
+        } else if (cantidad > saldo) {
+            System.out.println("Saldo insuficiente.");
+        } else {
+            saldo -= cantidad;
+            System.out.println("Retiro realizado con éxito. Su nuevo saldo es: $" + saldo);
+        }
+    }
+
+    public static void cambiarPIN() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese su PIN actual: ");
+        int pinIngresado = scanner.nextInt();
+
+        if (pinIngresado == pinActual) {
+            System.out.print("Ingrese su nuevo PIN: ");
+            int nuevoPin = scanner.nextInt();
+            System.out.print("Confirme su nuevo PIN: ");
+            int confirmacionPin = scanner.nextInt();
+
+            if (nuevoPin == confirmacionPin) {
+                pinActual = nuevoPin;
+                System.out.println("PIN actualizado con éxito.");
+            } else {
+                System.out.println("Los PINs no coinciden.");
+            }
+        } else {
+            System.out.println("PIN incorrecto.");
+        }
     }
 }
