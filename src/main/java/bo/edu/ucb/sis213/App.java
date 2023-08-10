664a0044835c4896c4c8915678318ee60db2720c
Ernesto Campohermoso <ecampohermoso@cirrus-it.net>
2023-08-10 19:15:17 -0400

    Primera versión del proyecto

---
+++ src/main/java/bo/edu/ucb/sis213/App.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.sis213;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
